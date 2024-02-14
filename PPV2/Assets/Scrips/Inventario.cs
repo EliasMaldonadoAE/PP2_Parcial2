069bbb97a01eb0af960e230d8a0b5af14265f0e2
Chharli <144072056+Chharli@users.noreply.github.com>
2024-02-13 19:59:03 -0600

    Inventory-UI
    13-Feb-2024

---
+++ PPV2/Assets/Scrips/Inventario.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour
+{
+    public int ActiveSlot = 0;
+    public Transform Slots;
+    public Transform[] InventorySlots;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InventorySlots = Slots.GetComponentsInChildren<Transform>();
+        InventorySlots[ActiveSlot].GetChild(0).gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InventorySlots[ActiveSlot].GetChild(0).gameObject.SetActive(true);
+    }
+}
